043d085a43dde271177897a45d0b4d7fa4fd9de0
MeisnerMax <143499606+MeisnerMax@users.noreply.github.com>
2025-03-28 14:49:42 +0100

    Update ServiceCard.js

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -5,7 +5,7 @@ export default function ServiceCard({ icon, title, description, onClick }) {
       onClick={onClick}
       className="block group transform transition duration-300 hover:-translate-y-1 hover:shadow-2xl cursor-pointer"
     >
-      <div className="bg-black rounded-lg overflow-hidden shadow-2xl p-6 text-center">
+      <div className="bg-blue-900 rounded-lg overflow-hidden shadow-2xl p-6 text-center">
         <div className="group-hover:animate-pulse">{icon}</div>
         <h4 className="text-xl font-bold my-4 text-white">{title}</h4>
         <p className="text-white">{description}</p>
